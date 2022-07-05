2bb59d452c3dc6a6ede2103e8d3a6b5ee08809d4
Gabriel Bernardo <bielcotrimsv@gmail.com>
2022-07-04 21:25:33 -0300

    resolução do requisito 2

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,56 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { createUser } from '../services/userAPI';
+import Loading from '../components/Loading';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      nome: '',
+      loading: false,
+    };
+  }
+
+  handleClick = async () => {
+    const { nome } = this.state;
+    const { history } = this.props;
+    this.setState({ loading: true });
+    await createUser({ name: nome });
+    history.push('/search');
+  }
+
   render() {
+    const { nome, loading } = this.state;
     return (
       <div data-testid="page-login">
-        <h1>Home</h1>
+        <form>
+          <input
+            type="text"
+            name="name"
+            value={ nome }
+            onChange={ ({ target }) => this.setState({ nome: target.value }) }
+            placeholder="Insira seu nome"
+            data-testid="login-name-input"
+          />
+          <button
+            type="button"
+            name="button"
+            onClick={ this.handleClick }
+            data-testid="login-submit-button"
+            disabled={ nome.length <= 2 }
+          >
+            Entrar
+          </button>
+          { loading && <Loading /> }
+        </form>
       </div>
     );
   }
 }
+
+Home.propTypes = {
+  history: PropTypes.arrayOf(PropTypes.func).isRequired,
+};
 
 export default Home;
