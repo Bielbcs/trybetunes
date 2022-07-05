7740af5f57fb08ef305ae84ef4fa965f25ff0103
Gabriel Bernardo <bielcotrimsv@gmail.com>
2022-07-04 22:36:56 -0300

    resolução do requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
+import '../styles/Header.css';
 
 class Header extends React.Component {
   constructor() {
@@ -26,13 +28,30 @@ class Header extends React.Component {
   render() {
     const { username, loading } = this.state;
     return (
-      <header data-testid="header-component">
+      <header data-testid="header-component" className="header-container">
         { loading ? <Loading /> : (
           <span
             data-testid="header-user-name"
           >
             { username.name }
           </span>) }
+        <div className="links-container">
+          <Link to="/search" data-testid="link-to-search" className="link">Search</Link>
+          <Link
+            to="/favorites"
+            data-testid="link-to-favorites"
+            className="link"
+          >
+            Favorites
+          </Link>
+          <Link
+            to="/profile"
+            data-testid="link-to-profile"
+            className="link"
+          >
+            Profile
+          </Link>
+        </div>
       </header>
     );
   }
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Loading extends React.Component {
   render() {
-    return <div>Carregando...</div>;
+    return <h3 className="loading">Carregando...</h3>;
   }
 }
 
